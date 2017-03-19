bba88faf6175e7454de0260a28d9e1ec8ad6a934
ThomsonTang <guiketang@gmail.com>
2017-03-19 23:29:45 +0800

    add python cookbook

---
+++ learn-python-hard-way/src/ex11.py
@@ -0,0 +1,12 @@
+# Asking Questions
+
+print 'How old are you?'
+age = raw_input()
+
+print "age: ", age
+
+# Prompt People
+
+height = raw_input("How tall are you?")
+print "you are %s tall." % height
+print "you are %r tall." % height
